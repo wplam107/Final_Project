4850e02e000cf99dec69553463a283a108eea031
Wayne <w.p.lam107@gmail.com>
2020-04-04 00:58:16 -0400

    update ws_func

--- ws_func.py
+++ ws_func.py
@@ -7,6 +7,8 @@ import datetime
 import random
 import requests
 import pickle
+import urllib
+from bs4 import BeautifulSoup
 from os import system
 from datetime import datetime
 from selenium import webdriver
@@ -18,61 +20,38 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
 # CNN FUNCTIONS
-def scrape_urls_cnn(urls, dates, start, stop, driver):
-    '''
-    Function to scrape all urls of topic
-    '''
-    time.sleep(2)
-    xpath = '/html/body/div[5]/div[2]/div/div[2]/div[2]/div/div[3]/div[{}]/div[2]/div[1]/span[2]'
-    a_xpath = '/html/body/div[5]/div[2]/div/div[2]/div[2]/div/div[3]/div[{}]/div[2]/h3/a'
-    ymd_start = datetime(start[0], start[1], start[2]).date()
-    ymd_stop = datetime(stop[0], stop[1], stop[2]).date()
-    
-    # Scrape urls of each page
-    for i in range(10):
-        counter = 0
-        try:
-            ele = driver.find_element_by_xpath(xpath.format(i+1)).text.replace(',', '')
-            ts = datetime.strptime(ele, '%b %d %Y').date()
-            href = driver.find_element_by_xpath(a_xpath.format(i+1)).get_attribute('href')
-            if ts < ymd_start:
-                continue
-            elif ts < ymd_stop:
-                if re.search('(live-news)', href):
-                    continue
-                else:
-                    urls.append(href)
-                    dates.append(ts)
-        except:
+def urls_scrape_page_cnn(urls, page, driver):
+    driver.get(page)
+    time.sleep(5)
+    xpath = '/html/body/div[5]/div[2]/div/div[2]/div[2]/div/div[3]/div[{}]/div[2]/h3/a'
+    counter = 0
+    for i in range(1, 11):
+        href = driver.find_element_by_xpath(xpath.format(i)).get_attribute('href')
+        if re.search('live-news', href):
+            continue
+        else:
             counter += 1
-            if counter < 5:
-                continue
-            else:
-                print('All URLS in date range scraped')
-                print(f'Number of article URLs: {len(urls)}\n')
-                return urls, dates
-    
-    # Click to next page if there is one
-    try:
-        np_xpath= '/html/body/div[5]/div[2]/div/div[2]/div[2]/div/div[5]/div/div[3]'
-        driver.find_element_by_xpath(np_xpath).click()
-    except:
-        print('No more pages')
-        print(f'Number of article URLs: {len(urls)}\n')
-        return urls, dates
+            urls.append(href)
+    print(f'Added {counter} URLs')
+    return urls
 
-    return scrape_urls_cnn(urls, dates, start, stop, driver)
+def urls_scrape_all_cnn(driver):
+    urls = []
+    search_site = ['https://www.cnn.com/search?size=10&q=%22hong%20kong%20protests%22&type=article']
+    s_pages = 'https://www.cnn.com/search?size=10&q=%22hong%20kong%20protests%22&page={}&from={}0&type=article'
+    pages = search_site + [ s_pages.format(i, i-1) for i in range(2, 17) ]
+    for page in pages:
+        urls = urls_scrape_page_cnn(urls, page, driver)
+    return urls
 
 # Helper function to clean body of article
 def _clean_body(article):
     cleaned = re.sub(r"^.*?\)|(CNN's.*)", "", article)
     return cleaned
 
-def scrape_one_cnn(url, driver, count_sc, count_no):
-    '''
-    Function to scrape headline and article body
-    '''
+def scrape_one_cnn(url, counter, driver):
     driver.get(url)
+    body = []
     time.sleep(3)
 
     # Click modal button
@@ -81,80 +60,60 @@ def scrape_one_cnn(url, driver, count_sc, count_no):
         modal_button.click()
     except:
         pass
-
+    
+    try:
+        date = driver.find_element_by_class_name('update-time').text
+    except:
+        date = ''
     try:
         headline = driver.find_element_by_class_name('pg-headline').text
-        texts = driver.find_elements_by_class_name('zn-body__paragraph')
-        article = ' '.join([ text.text for text in texts ])
-        article = _clean_body(article)
-        count_sc += 1
     except:
-        count_no += 1
-        headline = 'No headline'
-        article = 'No text'
+        headline = ''
+    try:
+        texts = driver.find_elements_by_class_name('zn-body__paragraph')
+        for text in texts:
+            if re.search('CNN\'s', text.text):
+                continue
+            else:
+                body.append(text.text)
+        counter += 1
+    except:
+        body.append('')
+    
+    body = ' '.join(body)
         
-    return headline, article, count_sc, count_no  
+    return url, date, headline, body, counter
 
-def scrape_articles_cnn(urls, dates, headlines, bodies, start, stop, driver):
-    '''
-    Function to scrape all designated articles
-    '''
-    urls, dates = scrape_urls_cnn(urls, dates, start, stop, driver)
-    
-    count_sc = 0
-    count_no = 0
-
-    for url in urls:
-        headline, article, count_sc, count_no = scrape_one_cnn(url, driver, count_sc, count_no)
-        bodies.append(article)
-        headlines.append(headline)
-    
-    print(f'Number of Articles Scraped: {count_sc}\n')
-    print(f'Number of Articles w/o Text: {count_no}\n')
-
-    return urls, dates, headlines, bodies      
-
-def scrape_cnn(start, stop, ret_csv=False, csv='', ret_df=True):
-    '''
-    Function to convert data to DataFrame and .csv
-
-    Parameters:
-        start : tuple (Y, m, d), inclusive start date for article scraping
-        stop : tuple (Y, m, d), exclusive stop date for article scraping
-        ret_csv : bool (default False), True returns .csv file
-        csv : str (path), path for .csv file if ret_csv=True
-        ret_df : bool (default False), True returns DataFrame
-    '''
-    # Instantiate driver
-    driver = webdriver.Chrome()
-    driver.get('https://www.cnn.com/search?q=hong%20kong%20protests&size=10&page=1&category=world&type=article')
-
-    urls = []
+def scrape_cnn(urls, driver, ret_csv=False, csv=''):
+    new_urls = []
     dates = []
     headlines = []
     bodies = []
     
-    urls, dates, headlines, bodies = scrape_articles_cnn(urls, dates, headlines, bodies, start, stop, driver)
+    counter = 0
+    
+    for url in urls:
+        time.sleep(1)
+        url, date, headline, body, counter = scrape_one_cnn(url, counter, driver)
+        new_urls.append(url)
+        dates.append(date)
+        headlines.append(headline)
+        bodies.append(body)
+        if counter % 10 == 0:
+            print(f'Articles scraped so far: {counter}')
+            time.sleep(2)
+    
     df = pd.DataFrame()
-    df['url'] = urls
+    df['url'] = new_urls
     df['date'] = dates
     df['headline'] = headlines
     df['body'] = bodies
     df['source'] = 'CNN'
-    df['index'] = range(len(df.index))
-    df.set_index('index', inplace=True)
-
-    # Quit driver
-    driver.quit()
     
-    # Convert to .csv (with tab delimiter)
     if ret_csv == True:
         df.to_csv(csv, sep='\t')
         print(f'File {csv} Created')
-    
-    if ret_df == True:
-        return df
-    return
+    return df
 
 # SCMP functions
 
@@ -294,19 +253,19 @@ def scrape_articles_scmp(urls, headlines, bodies):
     count_no = 0
 
     for url in urls:
-        time.sleep(random.uniform(0, 2))
+        time.sleep(random.uniform(1, 3))
         headline, article, count_sc, count_no = scrape_one_scmp(url, driver, count_sc, count_no)
         bodies.append(article)
         headlines.append(headline)
 
         if (count_sc + count_no) % 25 == 0 and (count_sc + count_no) != 0:
             driver.quit()
-            time.sleep(15)
+            time.sleep(20)
             driver = webdriver.Chrome()
             driver.maximize_window()
-            time.sleep(15)
+            time.sleep(20)
         if (count_sc + count_no) % 50 == 0 and (count_sc + count_no) != 0:
-            time.sleep(random.uniform(8, 12))
+            time.sleep(10)
             print(f'Current articles scraped: {count_sc + count_no}')
         if (count_sc + count_no) % 250 == 0 and (count_sc + count_no) != 0:
             hl_file = open(f'scmp_hl{count_sc + count_no}.p', 'wb')
@@ -381,84 +340,222 @@ def url_scrape_all_abc(driver):
     print(f'Total Articles: {len(urls)}')
     return urls
 
-def scrape_one_abc(url, driver, count_sc, count_no):
-    driver.get(url)
-    time.sleep(2)
-    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-    time.sleep(2)
-
-    try:
-        date = driver.find_element_by_css_selector('time').get_attribute('title')
-        date = datetime.strptime(date.split(',')[0], '%a %d %b %Y').date()
-    except:
-        date = driver.find_element_by_css_selector('time').text
-        date = ' '.join(date.split(' ')[1:4])
+def bs_scrape_body_abc(url):
+    print(url)
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    article = soup.find('div', class_='article section')
+    if article == None:
+        article = soup.find('article')
+    if article == None:
+        article = soup.find('div', class_='comp-rich-text article-text clearfix')
+    ps = article.find_all('p')
+    date = article.find('span', class_='timestamp')
+    if date == None:
+        date = article.find('time')['datetime']
+    else:
+        date = date.get_text()
         try:
-            date = datetime.strptime(date, '%d %B %Y').date()
+            date = re.findall(r'([a-zA-Z]+\s\d+\,\s\d+)', date)[0]
         except:
-            date = datetime.strptime(date, '%d %b %Y').date()
-
-    headline = driver.find_element_by_css_selector('h1').text
-    eles = driver.find_elements_by_xpath('//*[@id="main_content"]/div[1]/div/div[1]/div/p')
+            date = re.findall(r'([a-zA-Z]+\s\d+\s\d+)', date)[0]
+    headline = article.find('h1').string
     body = []
-    for ele in eles:
-        p = ele.text
-        cond1 = re.search('Topics:', p)
-        cond2 = re.search('Updated', p)
-        cond3 = re.search('First posted', p)
-        cond4 = re.search('Source:', p)
-        if cond1 or cond2 or cond3 or cond4 or p == '':
+    for p in ps:
+        text = p.get_text()
+        cond1 = re.search('Updated', text)
+        cond2 = re.search('Topics:', text)
+        cond3 = re.search('First posted', text)
+        cond4 = re.search('Posted', text)
+        cond5 = re.search('Source:', text)
+        if cond1 or cond2 or cond3 or cond4 or cond5:
             continue
         else:
-            body.append(p)
-
+            body.append(text)
     body = ' '.join(body)
-    count_sc += 1
-    if count_sc % 10 == 0:
-        print(f'Articles scraped so far: {count_sc}')
-    return date, headline, body, count_sc, count_no
+    return url, headline, date, body
 
-def scrape_all_abc(urls, ret_csv=False, csv='', ret_df=True):
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-
-    dates = []
+def bs_scrape_abc(urls, ret_csv=False, csv=''):
+    new_urls = []
     headlines = []
+    dates = []
     bodies = []
-    urls_sc = []
-
-    count_sc = 0
-    count_no = 0
-
+    source = 'ABC (Australia)'
+    
+    counter = 0
+    
     for url in urls:
-        if re.search('interactive', url) or re.search('documentary', url):
-            count_no += 1
-            print(f'Articles bypassed so far: {count_no}')
+        cond1 = re.search('interactive', url)
+        cond2 = re.search('documentary', url)
+        cond3 = re.search('the-world', url)
+        cond4 = re.search('newschannel', url)
+        cond5 = re.search('science', url)
+        if cond1 or cond2 or cond3 or cond4 or cond5:
             continue
         else:
-            date, headline, body, count_sc, count_no = scrape_one_abc(url, driver, count_sc, count_no)
-            urls_sc.append(url)
-            dates.append(date)
+            time.sleep(random.uniform(1, 2))
+            url, headline, date, body = bs_scrape_body_abc(url)
+            new_urls.append(url)
             headlines.append(headline)
+            dates.append(date)
             bodies.append(body)
+            counter += 1
+            if counter % 10 == 0:
+                print(f'Scraped so far: {counter}')
     
-    driver.quit()
-    print(f'Articles Scraped: {count_sc}')
-    print(f'Articles Bypasses: {count_no}')
-
     df = pd.DataFrame()
-    df['url'] = urls_sc
+    df['url'] = new_urls
     df['date'] = dates
     df['headline'] = headlines
     df['body'] = bodies
-    df['source'] = 'ABC (Australia)'
-    df['index'] = range(len(urls_sc))
-    df.set_index('index', inplace=True)
-
+    df['source'] = source
+    
     if ret_csv == True:
         df.to_csv(csv, sep='\t')
         print(f'File {csv} Created')
     
-    if ret_df == True:
-        return df
-    return
+    return df
+
+# CCTV Functions
+
+def url_scrape_page_cctv(urls, page, counter):
+    link = urllib.request.urlopen(page)
+    soup = BeautifulSoup(link, 'html.parser')
+    heads = soup.find_all('h1')
+    for head in heads:
+        href = head.find('a').get('href')
+        urls.append(href)
+        counter += 1
+    return urls, counter
+
+def url_scrape_all_cctv():
+    urls = []
+    cctv_search = 'http://so.cntv.cn/language/english/index.php?qtext=hong+kong+protest&type=1&sort=SCORE&page={}&vtime=-1&datepid=5&history=yes'
+    pages = [ cctv_search.format(i) for i in range(1, 32) ]
+    counter = 0
+    for page in pages:
+        urls, counter = url_scrape_page_cctv(urls, page, counter)
+        if counter % 10 == 0:
+            print(f'URLs scraped: {counter}')
+    print(f'Total URLs scraped: {counter}')
+    return urls
+
+def scrape_body_cctv(url):
+    body = []
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    page_body = soup.body
+    headline = page_body.h3.get_text()
+    headline = page_body.find('h3').get_text()
+    date = page_body.find('h3').find_next_sibling('p').find_next_sibling('p').get_text()[:10]
+    text_body = page_body.find(class_='text')
+    ps = text_body.find_all('p')
+    for p in ps:
+        if re.search('Photo', p.get_text()):
+            continue
+        else:
+            body.append(p.get_text())
+    body = ' '.join(body)
+    return url, date, headline, body
+
+def scrape_cctv(urls, ret_csv=False, csv=''):
+    new_urls = []
+    dates = []
+    headlines = []
+    bodies = []
+    
+    counter = 0
+    
+    for url in urls:
+        time.sleep(1)
+        url, date, headline, body = scrape_body_cctv(url)
+        new_urls.append(url)
+        dates.append(date)
+        headlines.append(headline)
+        bodies.append(body)
+        counter += 1
+        if counter % 25 == 0:
+            print(f'Articles scraped: {counter}')
+    
+    df = pd.DataFrame()
+    df['url'] = new_urls
+    df['date'] = dates
+    df['headline'] = headlines
+    df['body'] = bodies
+    df['source'] = 'CCTV'
+    
+    if ret_csv == True:
+        df.to_csv(csv, sep='\t')
+        print(f'File {csv} Created')
+    
+    return df
+
+# Reuters functions
+
+# Function to clickdown for more articles till no more
+def clickdown_reuters(driver):
+    more_button = driver.find_element_by_xpath('//*[@id="content"]/section[2]/div/div[1]/div[4]/div/div[4]/div[1]')
+    for i in range(76):
+        time.sleep(0.5)
+        try:
+            more_button.click()
+        except:
+            break
+
+def url_scrape_reuters(driver):
+    eles = driver.find_elements_by_css_selector('h3')
+    urls = [ ele.find_element_by_css_selector('a').get_attribute('href') for ele in eles ]   
+    return urls
+
+def scrape_one_reu(url):
+    page = urllib.request.urlopen(url)
+    time.sleep(1.5)
+    soup = BeautifulSoup(page, 'html.parser')
+    headline = soup.find('h1').get_text()
+    date = soup.find('div', class_='ArticleHeader_date').get_text()
+    text_body = soup.find('div', class_='StandardArticleBody_body')
+    texts = text_body.find_all('p')
+    body = []
+    for text in texts:
+        cond1 = re.search('Writing by', text.get_text())
+        cond2 = re.search('Editing by', text.get_text())
+        cond3 = re.search('Reporting by', text.get_text())
+        if cond1 or cond2 or cond3:
+            continue
+        else:
+            body.append(text.get_text())
+    body = ' '.join(body)
+    return url, date, headline, body
+
+def scrape_reuters(urls, ret_csv=False, csv=''):
+    new_urls = []
+    dates = []
+    headlines = []
+    bodies = []
+    
+    counter = 0
+    
+    for url in urls:
+        time.sleep(random.uniform(0.5, 1.5))
+        url, date, headline, body = scrape_one_reu(url)
+        new_urls.append(url)
+        dates.append(date)
+        headlines.append(headline)
+        bodies.append(body)
+        counter += 1
+        if counter % 25 == 0:
+            print(f'Articles scraped: {counter}')
+    
+    df = pd.DataFrame()
+    df['url'] = new_urls
+    df['date'] = dates
+    df['headline'] = headlines
+    df['body'] = bodies
+    df['source'] = 'Reuters'
+    print(f'Total Articles Scraped: {counter}')
+    
+    if ret_csv == True:
+        df.to_csv(csv, sep='\t')
+        print(f'File {csv} Created')
+    
+    return df
