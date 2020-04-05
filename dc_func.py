93b0be1f18a2961ee49c0687e91bc44ecde44593
Wayne <w.p.lam107@gmail.com>
2020-04-05 13:26:53 -0400

    temp files and update

--- dc_func.py
+++ dc_func.py
@@ -7,7 +7,8 @@ import pickle
 def not_HK(df):
     cond1 = (df['headline'].str.find('Hong Kong') == -1) & (df['body'].str.find('Hong Kong') == -1)
     cond2 = (df['headline'].str.find('Hong Kong') == -1) & (df['body'].str.find('Hong Kong') > 1000)
-    a = np.where(cond1 | cond2, df.index, 0)
+    cond3 = (df['body'].str.find('TODAYS') != -1)
+    a = np.where((cond1) | (cond2) | cond3, df.index, 0)
     b = [ i for i in a if i != 0 ]
     df.drop(b, inplace=True)
     return df
@@ -33,8 +34,8 @@ def clean_body_cctv(s):
         match = re.search(r'(?<=\s--\s).*$', s).group(0)
     except:
         match = s
-
     match = match.replace('\'', '')
+    match = match.replace('\n', '')
     match = re.sub(r'\(.*?\)', '', match)
     return match
 
@@ -48,6 +49,7 @@ def con_to_string(s):
 
 def clean_body_abc(s):
     match = s.replace('\'', '')
+    match = re.sub(r'\(.*?\)', '', match)
     return match
 
 # CNN clean funtions
@@ -56,7 +58,10 @@ def string_to_date_cnn(s):
     return match
 
 def clean_body_cnn(s):
-    match = re.search(r'(?<=\))(.*)', s).group(0)
+    try:
+        match = re.search(r'(?<=\))(.*)', s).group(0)
+    except:
+        match = s
     match = match.replace('\'', '')
     match = re.sub(r'\(.*?\)', '', match)
     return match
